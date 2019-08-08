5a4f718e81870d03da12774181d13484741ee69c
Domenico Maisano <dev.dmaisano@gmail.com>
2019-08-08 19:55:19 -0400

    added env file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className="App antialiased">
-      <h1 className="text-link font-semibold text-indigo-500">React App</h1>
+      <h1 className="text-2xl font-semibold text-indigo-500">React App</h1>
     </div>
   );
 };
