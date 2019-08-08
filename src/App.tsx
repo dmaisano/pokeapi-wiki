018e2edc7282d173a75178c497b7221bb881c9b5
Domenico Maisano <dev.dmaisano@gmail.com>
2019-08-08 19:50:35 -0400

    added webpack override for postcss plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App antialiased">
+      <h1 className="text-link font-semibold text-indigo-500">React App</h1>
     </div>
   );
-}
+};
 
 export default App;
