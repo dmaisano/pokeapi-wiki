89731b05af55068ba73990873e813b683a934c5c
Domenico Maisano <dev.dmaisano@gmail.com>
2019-08-12 15:15:33 -0400

    removed react tsx project

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const tailwind = require('tailwindcss');
-const { join } = require('path');
-
-module.exports = {
-  plugins: [
-    tailwind(join(__dirname, './tailwind.config.js')),
-    require('autoprefixer'),
-  ],
-};
